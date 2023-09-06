3b545292207c3032dafa13f2aeecf7c3a2608d4d
yasircb <78907008+yasircb@users.noreply.github.com>
2023-09-06 18:45:44 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import Darwin.C
 
 let package = Package(
     name: "Facebook",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v12),.visionOS(.v1)],
     products: [
         // The Kernel of the SDK. Must be included as a runtime dependency.
         .basics,
